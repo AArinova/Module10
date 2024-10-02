1ab7097db8dee56cfe6e7c8defa8dd40cafa4866
AArinova <aliya-arinova@yandex.ru>
2024-10-02 19:55:10 +0700

    Add files via upload

--- module_10_1.py
+++ module_10_1.py
@@ -7,33 +7,33 @@ def write_words(word_count, file_name):
         for i_word in range(1, word_count + 1):
             print(f"Какое-то слово № {i_word}.", file=file)
             sleep(0.1)
-        print(f"Завершилась запись в файл {file_name}.", file=file)
+    print(f"Завершилась запись в файл {file_name}.")
 
 start = datetime.now()
 
 write_words(10, 'example1.txt')
-write_words(30, 'example1.txt')
-write_words(200, 'example1.txt')
-write_words(100, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
 
 stop = datetime.now()
 print('Время выполнения :', stop - start)
 
 start = datetime.now()
 
-t1 = Thread( target=write_words, args=(10, 'example1.txt'))
+t1 = Thread( target=write_words, args=(10, 'example5.txt'))
 t1.start()
 t1.join()
 
-t2 = Thread( target=write_words, args=(30, 'example1.txt'))
+t2 = Thread( target=write_words, args=(30, 'example6.txt'))
 t2.start()
 t2.join()
 
-t3 = Thread( target=write_words, args=(200, 'example1.txt'))
+t3 = Thread( target=write_words, args=(200, 'example7.txt'))
 t3.start()
 t3.join()
 
-t4 = Thread( target=write_words, args=(100, 'example1.txt'))
+t4 = Thread( target=write_words, args=(100, 'example8.txt'))
 t4.start()
 t4.join()
 
