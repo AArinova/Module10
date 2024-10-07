9c5e1455ea2943f4f96738ab6b768e0f77e0ff1e
AArinova <aliya-arinova@yandex.ru>
2024-10-07 19:14:29 +0700

    Add files via upload

--- module_10_4.py
+++ module_10_4.py
@@ -1,57 +1,59 @@
-import queue
+from queue import Queue
 from threading import Thread
 from random import randint
 from time import sleep
 
-
 class Table:
 
-    def __init__(self, number: int, guest = None):
+    def __init__(self, number: int):
         self.number = number
-        self.guest = guest
-
-    """усаживаем гостя за стол"""
-    def sit_guest(self, guest_name: str):
-        if self.guest != None:
-            return False
-        else:
-            self.guest = guest_name
-            return True
+        self.guest = None
 
 class Guest(Thread):
 
-    def __idiv__(self, name: str):
+    def __init__(self, name: str):
+        super().__init__()
         self.name = name
 
     def run(self):
         """случайное ожидание"""
         wait = randint(3, 10)
-        print('Задержка', wait)
         sleep(wait)
-
 class Cafe:
 
-    def __init__(self, queue: queue, tables: list):
-        self.queue = queue
+    def __init__(self, *tables):
+        self.queue = Queue()
         self.tables = tables
 
-    def guest_arrival(self, guests: list):
+    def guest_arrival(self, *guests):
         """прибытие гостей"""
         for i_guest in guests:
+            """гость не усажен за стол"""
             is_sit = False
             for i_table in self.tables:
-                if i_table.sit_quest(i_guest.name) == True:
+                if i_table.guest is None:
                     is_sit = True
+                    i_table.guest = i_guest
+                    i_guest.start()
                     print(f"{i_guest.name} сел(-а) за стол номер {i_table.number}.")
                     break
-            if is_sit == False:
+            if not is_sit:
                 self.queue.put(i_guest)
                 print(f"{i_guest.name} в очереди.")
 
-
     def discuss_guests(self):
-        """обслужитвание гостей"""
-        pass
+        """обслуживание гостей"""
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
+            for i_table in self.tables:
+                if i_table.guest is not None and not i_table.guest.is_alive():
+                    print(f'{i_table.guest.name} покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {i_table.number} свободен')
+                    i_table.guest = None
+                if i_table.guest is None and not self.queue.empty():
+                    guest = self.queue.get()
+                    i_table.guest = guest
+                    guest.start()
+                    print(f'{guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {i_table.number}')
 
 # Создание столов
 tables = [Table(number) for number in range(1, 6)]
@@ -67,4 +69,4 @@ cafe = Cafe(*tables)
 # Приём гостей
 cafe.guest_arrival(*guests)
 # Обслуживание гостей
-#cafe.discuss_guests()
+cafe.discuss_guests()
